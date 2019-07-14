af607f0ebd148e473a9dc3a97d1c33a351f08c35
Yephim <49190537+Yephim@users.noreply.github.com>
2019-07-15 01:19:03 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="jim_test_server",
+      version="0.2",
+      description="JSON Instant Messenger Server part",
+      author="Efim Sudov",
+      author_email="Efim.Sudov@nw.rt.ru",
+      url="https://github.com/Yephim/JIM_sever",
+      packages= find_packages()
+      )
